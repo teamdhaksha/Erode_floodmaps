7d80c6bd360151c618c8b3f0094d262fbd85f5d6
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-24 13:09:31 +0530

    Add files via upload

---
+++ data/Towns_6.js
@@ -0,0 +1,14 @@
+var json_Towns_6 = {
+"type": "FeatureCollection",
+"name": "Towns_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Erode" }, "geometry": { "type": "Point", "coordinates": [ 77.709541200837819, 11.345532041622461, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Bhavani" }, "geometry": { "type": "Point", "coordinates": [ 77.687110217097057, 11.452597770308101, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Chennimalai" }, "geometry": { "type": "Point", "coordinates": [ 77.602243773381389, 11.16585799158177, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Perundurai" }, "geometry": { "type": "Point", "coordinates": [ 77.581442285329047, 11.27879901174083, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Gobichettipalayam" }, "geometry": { "type": "Point", "coordinates": [ 77.43339454461271, 11.455389316115729, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Sathyamangalam" }, "geometry": { "type": "Point", "coordinates": [ 77.239916528678123, 11.497811863705691, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Anthiyur" }, "geometry": { "type": "Point", "coordinates": [ 77.58691074541251, 11.577933853061429, -999999.0 ] } }
+]
+}
